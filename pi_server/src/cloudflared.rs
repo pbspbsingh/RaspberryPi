d214a52a8f54fcd851a667c839ca2f0daa693381
Prashant Bhushan Singh <prashant.bhushansingh@gmail.com>
2021-03-01 22:45:13 -0800

    Now upading cloudflared every day at 2am

--- pi_server/src/cloudflared.rs
+++ pi_server/src/cloudflared.rs
@@ -1,6 +1,7 @@
 use std::process::Stdio;
 use std::time::Duration;
 
+use chrono::{Datelike, Local, NaiveDate};
 use tokio::io::AsyncReadExt;
 use tokio::process::Command;
 
@@ -55,39 +56,55 @@ pub struct Cloudflared<'a> {
 
 impl<'a> Cloudflared<'a> {
     pub async fn start_daemon(&self) -> anyhow::Result<()> {
-        self.update().await?;
-
-        log::info!(
-            "Starting cloudflared daemon: `{} proxy-dns --port {}`",
-            self.cmd,
-            self.port
-        );
-        let mut daemon = Command::new(&self.cmd)
-            .arg("proxy-dns")
-            .arg("--port")
-            .arg(self.port.to_string())
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
-            .kill_on_drop(true)
-            .spawn()?;
-        let mut stdout = daemon.stdout.take().unwrap();
-        let mut stderr = daemon.stderr.take().unwrap();
-        let mut buff = [0u8; 1024];
         loop {
-            match tokio::time::timeout(Duration::from_secs(5), stdout.read(&mut buff)).await {
-                Err(_) => {}
-                Ok(len) => log::info!("stdout: {}", String::from_utf8_lossy(&buff[..len?])),
-            }
-            match tokio::time::timeout(Duration::from_secs(5), stderr.read(&mut buff)).await {
-                Err(_) => {}
-                Ok(len) => log::info!("stderr: {}", String::from_utf8_lossy(&buff[..len?])),
-            }
-            if daemon.id().is_none() {
-                log::warn!("oops, cloudflared process has died");
-                break;
+            let rt = Local::now().naive_local();
+            let rt = NaiveDate::from_ymd(rt.year(), rt.month(), rt.day()).and_hms(2, 0, 0);
+            let rt = rt + chrono::Duration::days(1);
+            log::info!("Will restart the daemon at {}", rt);
+
+            self.update().await?;
+
+            log::info!(
+                "Starting cloudflared daemon: `{} proxy-dns --port {}`",
+                self.cmd,
+                self.port
+            );
+            let mut daemon = Command::new(&self.cmd)
+                .arg("proxy-dns")
+                .arg("--port")
+                .arg(self.port.to_string())
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .kill_on_drop(true)
+                .spawn()?;
+            let mut stdout = daemon.stdout.take().unwrap();
+            let mut stderr = daemon.stderr.take().unwrap();
+            let mut buff = [0u8; 1024];
+            loop {
+                let wait_time = Duration::from_secs(5);
+                if let Ok(len) = tokio::time::timeout(wait_time, stdout.read(&mut buff)).await {
+                    log::info!("stdout: {}", String::from_utf8_lossy(&buff[..len?]));
+                }
+                if let Ok(len) = tokio::time::timeout(wait_time, stderr.read(&mut buff)).await {
+                    log::info!("stderr: {}", String::from_utf8_lossy(&buff[..len?]));
+                }
+                if Local::now().naive_local() >= rt {
+                    log::info!("It's about time to restart the cloudflared daemon");
+                    daemon.kill().await.ok();
+                }
+                match daemon.try_wait() {
+                    Err(e) => {
+                        log::warn!("Something went wrong while trying to get the status of child process: {}", e);
+                        break;
+                    }
+                    Ok(Some(code)) => {
+                        log::warn!("daemon died with status {}", code);
+                        break;
+                    }
+                    Ok(None) => { /* Nothing to do */ }
+                }
             }
         }
-        Err(anyhow::anyhow!("cloudflared daemon died unexpectedly!"))
     }
 
     async fn update(&self) -> anyhow::Result<()> {
@@ -96,5 +113,19 @@ impl<'a> Cloudflared<'a> {
         buff += String::from_utf8_lossy(&update_output.stderr);
         log::info!("Updating cloudflared: {}", buff.trim());
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use chrono::{Datelike, Local, NaiveDate};
+
+    #[test]
+    fn test_datetime() {
+        let now = dbg!(Local::now().naive_local());
+        let next = NaiveDate::from_ymd(now.year(), now.month(), now.day() + 1).and_hms(2, 0, 0);
+
+        println!("Hmm {}", next);
+        println!("Kya {}, {}", now > next, next > now);
     }
 }
